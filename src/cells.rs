49be301e4756e28ba663d3d2569d23b1d9abe324
JanKaul <jankaul@mailbox.org>
2022-12-07 15:44:13 +0100

    create wit definition

--- src/cells.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Debug)]
-pub enum Cell {
-    Comment(String),
-    Code(String),
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,68 @@
-use cells::Cell;
+use self::dashbook_parser::{Cell, DashbookParser};
 use logos::Logos;
 use tokens::Token;
 
-mod cells;
 mod tokens;
 
-pub fn parse(input: &str) -> Vec<Cell> {
-    let mut lexer = Token::lexer(input);
-    let mut output = Vec::new();
-    let mut code = String::new();
-    while let Some(token) = lexer.next() {
-        match token {
-            Token::BeginComment => {
-                if !code.is_empty() {
-                    output.push(Cell::Code(code));
-                    code = String::new();
-                }
-                let mut comment = lexer
-                    .slice()
-                    .trim_start_matches(" ")
-                    .trim_start_matches("/*")
-                    .to_owned();
-                comment.push_str("\n");
-                while let Some(token) = lexer.next() {
-                    match token {
-                        Token::Line => {
-                            comment.push_str(lexer.slice());
-                            comment.push_str("\n");
-                        }
-                        Token::NewLine => comment.push_str(lexer.slice()),
-                        Token::EndComment => {
-                            comment.push_str(
-                                lexer
-                                    .slice()
-                                    .trim_end_matches(" ")
-                                    .trim_end_matches("\n")
-                                    .trim_end_matches("*/"),
-                            );
-                            comment.push_str("\n");
-                            break;
-                        }
-                        _ => (),
+wit_bindgen_guest_rust::generate!("dashbook-parser.wit");
+
+export_dashbook_parser!(DashbookParserImpl);
+struct DashbookParserImpl;
+
+impl DashbookParser for DashbookParserImpl {
+    fn parse(input: String) -> Vec<Cell> {
+        let mut lexer = Token::lexer(&input);
+        let mut output = Vec::new();
+        let mut code = String::new();
+        while let Some(token) = lexer.next() {
+            match token {
+                Token::BeginComment => {
+                    if !code.is_empty() {
+                        output.push(Cell::Code(code));
+                        code = String::new();
                     }
+                    let mut comment = lexer
+                        .slice()
+                        .trim_start_matches(" ")
+                        .trim_start_matches("/*")
+                        .to_owned();
+                    comment.push_str("\n");
+                    while let Some(token) = lexer.next() {
+                        match token {
+                            Token::Line => {
+                                comment.push_str(lexer.slice());
+                                comment.push_str("\n");
+                            }
+                            Token::NewLine => comment.push_str(lexer.slice()),
+                            Token::EndComment => {
+                                comment.push_str(
+                                    lexer
+                                        .slice()
+                                        .trim_end_matches(" ")
+                                        .trim_end_matches("\n")
+                                        .trim_end_matches("*/"),
+                                );
+                                comment.push_str("\n");
+                                break;
+                            }
+                            _ => (),
+                        }
+                    }
+                    output.push(Cell::Comment(comment))
                 }
-                output.push(Cell::Comment(comment))
+                Token::Line => {
+                    code.push_str(lexer.slice());
+                    code.push_str("\n");
+                }
+                Token::NewLine => code.push_str(lexer.slice()),
+                _ => (),
             }
-            Token::Line => {
-                code.push_str(lexer.slice());
-                code.push_str("\n");
-            }
-            Token::NewLine => code.push_str(lexer.slice()),
-            _ => (),
         }
+        if !code.is_empty() {
+            output.push(Cell::Code(code));
+        }
+        output
     }
-    if !code.is_empty() {
-        output.push(Cell::Code(code));
-    }
-    output
 }
 
 #[cfg(test)]
@@ -70,9 +76,10 @@ mod tests {
         Another cool comment*/
         let a = 5;
         a * 6
-        ";
+        "
+        .to_owned();
 
-        let cells = parse(input);
+        let cells = DashbookParserImpl::parse(input);
         match &cells[0] {
             Cell::Comment(comment) => {
                 assert_eq!(

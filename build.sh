ccbbc37d7ef0174e42d150db5085487367a873bc
Jan Kaul <jankaul@mailbox.org>
2023-11-08 09:18:06 +0100

    rename again

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ cargo build --release --target=wasm32-unknown-unknown
 wasm-tools component new \
   target/wasm32-unknown-unknown/release/dashbook_parser.wasm -o target/dashbook_parser.wasm
   
-jco transpile target/dashbook_parser.wasm --out-dir pkg --no-nodejs-compat --name index
+jco transpile target/dashbook_parser.wasm --out-dir pkg --no-nodejs-compat
